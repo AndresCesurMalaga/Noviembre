b298a63959fe546a7a6f2c12ae6a2a46adbd2d0b
AndresCesurMalaga <150119339+AndresCesurMalaga@users.noreply.github.com>
2023-11-22 12:53:36 +0100

    final

---
+++ ContadorInverso.java
@@ -0,0 +1,35 @@
+//1200 a 620
+//1 while
+//2 do while
+//3 for
+public class ContadorInverso {
+
+	public static void main(String[] args) {
+		
+		final int MAXIMO=1200;
+		final int MINIMO=620;
+		int x;
+		
+		x=MAXIMO;
+		while(x>MINIMO-20) {
+			System.out.print(x+" ");
+			x=x-20;
+		}
+		
+		System.out.println("while hecho");
+		
+		x=MAXIMO;
+		do{
+			System.out.print(x+" ");
+			x=x-20;
+		}while(x>MINIMO-20);
+		
+		System.out.println("do-while hecho");
+		
+		for(x=MAXIMO;x>MINIMO-20;x=x-20) {
+			System.out.print(x+" ");
+		}
+		
+		System.out.println("for hecho");
+	}
+}
---
+++ DiasSemanaV2.java
@@ -0,0 +1,82 @@
+
+public class DiasSemanaV2 {
+
+	public static void main(String[] args) {
+
+		// [L M X J V]
+		dibujarDiasAño();
+	}
+	
+	private static void dibujarDiasAño() {
+		char dia = 'L';
+		int semana = 0;
+		int meses = 0;
+		String mes = "Diciembre";
+
+		while (meses < 12) {
+			if (mes == "Diciembre") {
+				System.out.print("Enero ");
+				mes = "Enero";
+			} else if (mes == "Enero") {
+				System.out.print("Febrero ");
+				mes = "Febrero";
+			} else if (mes == "Febrero") {
+				System.out.print("Marzo ");
+				mes = "Marzo";
+			} else if (mes == "Marzo") {
+				System.out.print("Abril ");
+				mes = "Abril";
+			} else if (mes == "Abril") {
+				System.out.print("Mayo ");
+				mes = "Mayo";
+			} else if (mes == "Mayo") {
+				System.out.print("Junio ");
+				mes = "Junio";
+			} else if (mes == "Junio") {
+				System.out.print("Julio ");
+				mes = "Julio";
+			} else if (mes == "Julio") {
+				System.out.print("Agosto ");
+				mes = "Agosto";
+			} else if (mes == "Agosto") {
+				System.out.print("Septiembre ");
+				mes = "Septiembre";
+			} else if (mes == "Septiembre") {
+				System.out.print("Octubre ");
+				mes = "Octubre";
+			} else if (mes == "Octubre") {
+				System.out.print("Noviembre ");
+				mes = "Noviembre";
+			} else if (mes == "Noviembre") {
+				System.out.print("Diciembre ");
+				mes = "Diciembre";
+			}
+			
+			while (semana < 4) {
+				if (dia == 'L') {
+					System.out.print(" [");
+					System.out.print(" L");
+					dia = 'M';
+				} else if (dia == 'M') {
+					System.out.print(" M");
+					dia = 'X';
+				} else if (dia == 'X') {
+					System.out.print(" X");
+					dia = 'J';
+				} else if (dia == 'J') {
+					System.out.print(" J");
+					dia = 'V';
+				} else if (dia == 'V') {
+					System.out.print(" V");
+					System.out.print(" ]");
+					dia = 'L';
+					semana++;
+				}
+			}
+			System.out.println();
+			semana = 0;
+			meses++;
+		}
+	}
+
+}
